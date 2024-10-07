3e74d30416ae72668524561479bdca6fb33de72c
zekScript <armandas.latanauskas06@gmail.com>
2024-10-07 18:40:05 +0300

    else if uzd 2 DONE

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Scripting side
 
-let numerisVienas = 13;
-let numerisDu = 13;
+// let numerisVienas = 13;
+// let numerisDu = 13;
 
-if (numerisVienas > numerisDu){
-      console.log(`${numerisVienas} numeris yra didesnis nei ${numerisDu} numeris`);
+// if (numerisVienas > numerisDu){
+//       console.log(`${numerisVienas} numeris yra didesnis nei ${numerisDu} numeris`);
+// }
+// else if(numerisVienas === numerisDu){
+// console.log(`${numerisVienas} numeris toks pat kaip ${numerisDu} numeris`);
+// }
+// else{
+//       console.log(`${numerisVienas} numeris yra mazesnis nei ${numerisDu} numeris`);
+// }
+
+
+let asmensAmzius = 19;
+
+if (asmensAmzius <= 12){
+      console.log("Jus esate vaikas")
 }
-else if(numerisVienas === numerisDu){
-console.log(`${numerisVienas} numeris toks pat kaip ${numerisDu} numeris`);
+else if(asmensAmzius >= 13 && asmensAmzius <= 17){
+      console.log("Jus esate paauglys");
+}
+else if(asmensAmzius >= 18 && asmensAmzius <= 64){
+      console.log("Jus esate suauges");
+}
+else if(asmensAmzius >= 65){
+      console.log("Jus esate senjoras");
+}
+else if(asmensAmzius < 0){
+      console.log(`${asmensAmzius} negali buti maziau negi 0`)
 }
 else{
-      console.log(`${numerisVienas} numeris yra mazesnis nei ${numerisDu} numeris`);
+      console.log("Error (nera skaiciu)");
 }
