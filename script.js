fbfcde21f4f436afa2fe4a1b38fde059c44c2bee
zekScript <armandas.latanauskas06@gmail.com>
2024-10-07 18:25:14 +0300

    else if uzd 1 DONE

---
+++ script.js
@@ -0,0 +1,14 @@
+// Scripting side
+
+let numerisVienas = 13;
+let numerisDu = 13;
+
+if (numerisVienas > numerisDu){
+      console.log(`${numerisVienas} numeris yra didesnis nei ${numerisDu} numeris`);
+}
+else if(numerisVienas === numerisDu){
+console.log(`${numerisVienas} numeris toks pat kaip ${numerisDu} numeris`);
+}
+else{
+      console.log(`${numerisVienas} numeris yra mazesnis nei ${numerisDu} numeris`);
+}
