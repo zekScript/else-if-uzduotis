f50a4eb375565c954b38cb0777fe89f84333a9ec
zekScript <armandas.latanauskas06@gmail.com>
2024-10-07 18:53:08 +0300

    else if uzd 4 DONE

--- script.js
+++ script.js
@@ -14,23 +14,49 @@
 // }
 
 
-let asmensAmzius = 19;
+// let asmensAmzius = 19;
 
-if (asmensAmzius <= 12){
-      console.log("Jus esate vaikas")
+// if (asmensAmzius <= 12){
+//       console.log("Jus esate vaikas")
+// }
+// else if(asmensAmzius >= 13 && asmensAmzius <= 17){
+//       console.log("Jus esate paauglys");
+// }
+// else if(asmensAmzius >= 18 && asmensAmzius <= 64){
+//       console.log("Jus esate suauges");
+// }
+// else if(asmensAmzius >= 65){
+//       console.log("Jus esate senjoras");
+// }
+// else if(asmensAmzius < 0){
+//       console.log(`${asmensAmzius} negali buti maziau negi 0`)
+// }
+// else{
+//       console.log("Error (nera skaiciu)");
+// }
+
+
+let paz = -50;
+
+
+if (paz >= 90 && paz <= 100){
+      console.log("A")
 }
-else if(asmensAmzius >= 13 && asmensAmzius <= 17){
-      console.log("Jus esate paauglys");
+else if (paz >= 80 && paz <= 89){
+      console.log("B")
 }
-else if(asmensAmzius >= 18 && asmensAmzius <= 64){
-      console.log("Jus esate suauges");
+else if(paz >= 70 && paz <= 79){
+      console.log("C")
 }
-else if(asmensAmzius >= 65){
-      console.log("Jus esate senjoras");
+else if(paz >= 60 && paz <= 69){
+      console.log("D")
 }
-else if(asmensAmzius < 0){
-      console.log(`${asmensAmzius} negali buti maziau negi 0`)
+else if(paz >= 50 && paz <= 59){
+      console.log("E")
+}
+else if(paz >= 0 && paz <= 49){
+      console.log("Nepakankamas")
 }
 else{
-      console.log("Error (nera skaiciu)");
+      console.log("Neteisingai parasete skaiciu bandykite dar karta")
 }
